ae1b0c878e5797e2f20a18ba5d40810dfd24397d
Michał Itrych <michal.itrych1312@gmail.com>
2024-05-06 21:27:19 +0200

    fix

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -2,7 +2,6 @@ import Footer from "../components/global/Footer";
 import NavBar from "../components/global/NavBar";
 import NewsTile from "../components/forPages/NewsTile";
 import "./css/News.css";
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 
 const News = () => {
@@ -51,109 +50,4 @@ const News = () => {
     </>
   );
 };
-
-=======
-
-const News = () => {
-	const news = [
-		{
-			id: "1",
-			date: "28 kwietnia 2024",
-			title: "Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych",
-			desc: "Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!",
-		},
-		{
-			id: "2",
-			date: "28 kwietnia 2024",
-			title: "Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych",
-			desc: "Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!",
-		},
-		{
-			id: "3",
-			date: "28 kwietnia 2024",
-			title: "Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych",
-			desc: "Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!",
-		},
-		{
-			id: "4",
-			date: "28 kwietnia 2024",
-			title: "Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych",
-			desc: "Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!",
-		},
-	];
-	return (
-		<>
-			<NavBar />
-			<section className='news-wrapper'>
-				<div className='news-header'>
-					<h2>Aktualności</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur. Turpis tellus
-						gravida habitasse velit mi vitae. Risus varius egestas
-						scelerisque risus.
-					</p>
-				</div>
-				<div className='news-tiles'>
-					<NewsTile
-						date='28 kwietnia 2024'
-						title='Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych'
-						desc='Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!'
-						href='/news'
-					/>
-					<NewsTile
-						date='28 kwietnia 2024'
-						title='Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych'
-						desc='Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!'
-						href='/news'
-					/>
-					<NewsTile
-						date='28 kwietnia 2024'
-						title='Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych'
-						desc='Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!'
-						href='/news'
-					/>
-					<NewsTile
-						date='28 kwietnia 2024'
-						title='Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych'
-						desc='Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!'
-						href='/news'
-					/>
-					<NewsTile
-						date='28 kwietnia 2024'
-						title='Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych'
-						desc='Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!'
-						href='/news'
-					/>
-					<NewsTile
-						date='28 kwietnia 2024'
-						title='Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych'
-						desc='Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!'
-						href='/news'
-					/>
-					<NewsTile
-						date='28 kwietnia 2024'
-						title='Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych'
-						desc='Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!'
-						href='/news'
-					/>
-					<NewsTile
-						date='28 kwietnia 2024'
-						title='Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych'
-						desc='Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!'
-						href='/news'
-					/>
-					<NewsTile
-						date='28 kwietnia 2024'
-						title='Nowoczesne rozwiązania w instalacjach wodno-kanalizacyjnych'
-						desc='Odkryj innowacyjne rozwiązania dla efektywnych instalacji wodno-kanalizacyjnych!'
-						href='/news'
-					/>
-				</div>
-			</section>
-			<Footer />
-		</>
-	);
-};
-
->>>>>>> 31f0f8bcdad5ae2e83968a1db81779b67b16a2b2
 export default News;
